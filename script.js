cec4475165d009912f556c0fb82f16cd69d8d077
jvrluis09 <jvrearl09@gmail.com>
2025-09-04 20:34:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Handle Contact Form Submission
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent reload
+  alert("✅ Thank you for your message! I’ll get back to you soon.");
+  this.reset(); // clear form
+});
